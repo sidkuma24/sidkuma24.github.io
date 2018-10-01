abdfecd17d8bcfec4ff582b3a29a30cc3eeec8d8
Siddharth Kumar <siddharthkumar@Siddharths-Mbp.local>
2018-10-01 14:54:36 -0700

    added new resumes

---
+++ app.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var passport = require('passport');
+
+require('./api/models/db');
+require('./api/config/passport');
+
+app.use(passport.intialize());
+
+app.use('/api', routesApi);
